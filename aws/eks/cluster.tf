7887ea745be6189bd21160008398c49a617b9f0c
Ravi <ravi@vytec.com>
2023-03-28 17:29:13 +0530

    updated

--- aws/eks/cluster.tf
+++ aws/eks/cluster.tf
@@ -3,7 +3,7 @@
 #-----------------------
 
 resource "aws_eks_cluster" "eksdemo" {
-  name     = "${var.eks_cluster}"
+  name     = var.eks_cluster
   role_arn = aws_iam_role.eksdemorole.arn
 
   vpc_config {
