d9d69207471fdb7e311fe60cc8f57b638453b62a
Ravi <ravi@vytec.com>
2023-03-29 07:20:58 +0530

    updated

--- vpc.tf
+++ vpc.tf
@@ -38,3 +38,8 @@ resource "aws_instance" "foo" {
     cpu_credits = "unlimited"
   }
 }
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.foo.public_ip
+}
